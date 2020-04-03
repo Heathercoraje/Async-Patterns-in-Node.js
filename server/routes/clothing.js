42dba4a9528d781d93e0ae101df79e872956ad14
heathercoraje <pancontomateyjamon18@gmal.com>
2020-04-03 12:34:42 +0100

    refator: handle async with callback

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -6,12 +6,23 @@ const router = express.Router();
 /* GET all clothing */
 router.route('/')
   .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+    getClothingData((error, data) => {
+      if (error) console.log(error);
+      else {
+        console.log('returning clothing data');
+        res.send(data); 
+      }
+    });
+    console.log('doing more work')
   });
+
+async function getClothingData(callback) {
+  fs.readFile(datafile, 'utf8', (error, data) => {
+    if (error) callback(error, null);
+    else {
+      callback(null, data);
+    }
+  });
+};
 
 module.exports = router;
